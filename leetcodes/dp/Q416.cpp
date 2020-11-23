7187eef9aacf1beeac81efdd072a72a8a9f4b83e
CrIticLove <786322423@qq.com>
2020-11-24 00:26:32 +0800

    Q416状态压缩完成

--- leetcodes/dp/Q416.cpp
+++ leetcodes/dp/Q416.cpp
@@ -1,9 +1,45 @@
 #include <vector>
-#include <iostream>
 #include <algorithm>
 using namespace std;
 
 class Solution
+{
+public:
+    bool canPartition(vector<int> &nums)
+    {
+        if (nums.size() == 0 || nums.size() == 1)
+            return false;
+        int sum = 0;
+        for (auto num : nums)
+        {
+            sum += num;
+        }
+        if (sum % 2 != 0)
+            return false;
+        sum /= 2;
+        if (*max_element(nums.begin(), nums.end()) > sum)
+            return false;
+        vector<bool> dp(sum + 1, false);
+        vector<bool> dp_temp(dp);
+        dp[0] = true;
+        dp[nums[0]] = true;
+        for (int i = 1; i < nums.size(); i++)
+        {
+            dp_temp = dp;
+            for (int j = 1; j <= sum; j++)
+            {
+                if (nums[i] <= j)
+                {
+                    dp_temp[j] = dp[j] || dp[j - nums[i]];
+                }
+            }
+            dp = dp_temp;
+        }
+        return dp[sum];
+    }
+};
+
+class Solution_Without_State_Compress
 {
 public:
     bool canPartition(vector<int> &nums)
@@ -43,10 +79,3 @@ public:
         return dp[nums.size() - 1][sum];
     }
 };
-
-int main()
-{
-    vector<int> param{99, 1};
-    Solution solution;
-    solution.canPartition(param);
-}
