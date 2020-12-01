07331ff878e174183da939481d72eafe43167298
CrIticLove <786322423@qq.com>
2020-12-01 17:27:06 +0800

    Q34 朴实无华二分查找 added

--- leetcodes/array/Q34.cpp
+++ leetcodes/array/Q34.cpp
@@ -1,7 +1,9 @@
 #include <vector>
 #include <algorithm>
+#include <limits.h>
 using namespace std;
 
+// 朴实无华二分查找
 class Solution
 {
 public:
@@ -9,24 +11,28 @@ public:
     int flag;
     Solution()
     {
-        ans = vector<int>(2, -1);
+        ans = vector<int>{INT_MAX, INT_MIN};
     }
     vector<int> searchRange(vector<int> &nums, int target)
     {
         searchHelper(0, nums.size(), nums, target);
+        if (ans[0] == INT_MAX && ans[1] == INT_MIN)
+        {
+            ans = vector<int>(2, -1);
+        }
         return ans;
     }
-    void searchHelper(int start, int end, vector<int> &nums, int &target)
+    void searchHelper(int start, int end, vector<int> &nums, const int &target)
     {
         if (start >= end)
             return;
-        flag = (end - start) / 2;
+        flag = (end + start) / 2;
         if (nums[flag] == target)
         {
             ans[0] = min(ans[0], flag);
             ans[1] = max(ans[1], flag);
             searchHelper(start, flag, nums, target);
-            searchHelper(start + 1, end, nums, target);
+            searchHelper(flag + 1, end, nums, target);
         }
         else if (nums[flag] > target)
         {
