e739847ad0baa799ef2c0a0406108538f9e89b78
Jack <786322423@qq.com>
2020-12-03 20:28:16 +0800

    奇数筛 added

--- leetcodes/number/Q204.cpp
+++ leetcodes/number/Q204.cpp
@@ -43,3 +43,49 @@ public:
         return true;
     }
 };
+
+//  奇数筛
+class Solution_odd
+{
+public:
+    int countPrimes(int n)
+    {
+        if (n == 0 || n == 1 || n == 2)
+            return 0;
+        const int len = n / 2 - 1;
+        vector<bool> vec(len, true);
+        int temp;
+        long long t;
+        for (int i = 3; i < n; i += 2)
+        {
+            if (!(vec[(i - 3) / 2]))
+                continue;
+            if (isPrime(i))
+            {
+                for (t = (long long)(i) * (long long)(i); t <= n; t += 2 * i)
+                {
+                    vec[(t - 3) / 2] = false;
+                }
+            }
+        }
+        temp = 0;
+        for (bool b : vec)
+        {
+            if (b)
+            {
+                ++temp;
+            }
+        }
+        return temp + 1;
+    }
+
+    bool isPrime(int n)
+    {
+        for (int i = 2; i * i < n; ++i)
+        {
+            if (n % i == 0)
+                return false;
+        }
+        return true;
+    }
+};
