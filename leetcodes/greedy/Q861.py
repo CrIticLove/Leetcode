34ec54e18f261a2eecbd62639738b653df51b02a
Jack <786322423@qq.com>
2020-12-07 15:43:03 +0800

    Q861.py added

---
+++ leetcodes/greedy/Q861.py
@@ -0,0 +1,15 @@
+from typing import List
+
+
+class Solution:
+    def matrixScore(self, A: List[List[int]]) -> int:
+        height, width = len(A), len(A[0])
+        if width == 0:
+            return height
+        answer = height * pow(2, (width - 1))
+        for j in range(1, width):
+            counter = [0, 0]
+            for i in range(height):
+                counter[(A[i][j] + A[i][0]) % 2] += 1
+            answer += max(counter) * pow(2, (width - 1 - j))
+        return answer
