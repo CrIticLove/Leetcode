f036b54f46b7950b32da119404840bb5bfe68eea
Jack <786322423@qq.com>
2020-12-17 16:02:41 +0800

    Q136 added

---
+++ leetcodes/others/Q136.cpp
@@ -0,0 +1,15 @@
+#include <vector>
+using namespace std;
+
+class Solution
+{
+public:
+    int singleNumber(vector<int> &nums)
+    {
+        for (int i = 1; i < nums.size(); ++i)
+        {
+            nums[0] ^= nums[i];
+        }
+        return nums[0];
+    }
+};
