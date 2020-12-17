27105063170346b76050e042fabc3c2cd4e7cb66
Jack <786322423@qq.com>
2020-12-17 10:59:00 +0800

    Q

---
+++ leetcodes/others/Q121.cpp
@@ -0,0 +1,22 @@
+#include <vector>
+using namespace std;
+
+class Solution
+{
+public:
+    int maxProfit(vector<int> &prices)
+    {
+        const int len = prices.size();
+        if (len == 0 || len == 1)
+            return 0;
+        int small = 0, end = 1;
+        int ans = 0;
+        for (; end < len; ++end)
+        {
+            ans = max(ans, prices[end] - prices[small]);
+            if (prices[end] <= prices[small])
+                small = end;
+        }
+        return ans;
+    }
+};
